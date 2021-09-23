061882fae1268a967a89f350e854a676ed2037c8
Shivang Shingala <84413420+ShivangShingala@users.noreply.github.com>
2021-09-23 17:53:04 -0400

    Add files via upload
    Learned more about CSS and JS an implemented that to my previous project to make it better looking and better-functioning

---
+++ A-1 JS.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.body.style.backgroundColor = "maroon";
+    }
+function textColor() {
+    document.getElementById("demo").style.color = "red";
+    return false;
+    }
